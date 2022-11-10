70d6bed9e5bf161f1885c106bf5e5e588c09e7d8
Tytus <ty3131py-s@student.lu.se>
2022-11-10 15:19:54 +0100

    finnished script

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -3,8 +3,45 @@ import time
 import random
 import click
 
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.clear()
 
 def get_direction():
+    for event in sense.stick.get_events():
+        d_long = 0
+        d_la = 0
+        send_vel = False
+        c = click.getchar()
+        if c =='left':
+            click.echo('Left')
+            send_vel = True
+            d_long = -1
+            d_la = 0
+        elif c == 'right':
+            click.echo('Right')
+            send_vel = True
+            d_long = 1
+            d_la = 0
+        elif c =='up':
+            click.echo('Up')
+            send_vel = True
+            d_long = 0
+            d_la = 1
+        elif c == 'down':
+            click.echo('Down')
+            send_vel = True
+            d_long = 0
+            d_la = -1
+        else:
+            d_long = 0
+            d_la = 0
+            click.echo('Invalid input :(')
+            send_vel = False
+        return d_long, d_la, send_vel
+
+
+"""
     d_long = 0
     d_la = 0
     send_vel = False
@@ -35,7 +72,7 @@ def get_direction():
         click.echo('Invalid input :(')
         send_vel = False
     return d_long, d_la, send_vel
-
+"""
 
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5000/drone"
